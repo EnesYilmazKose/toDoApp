6bff4269aa9e59145a65cc49282183b7128704a1
Enes Yılmaz Köse <89836431+EnesYilmazKose@users.noreply.github.com>
2024-03-11 09:15:17 +0300

    commit-2

--- site.js
+++ site.js
@@ -2,8 +2,6 @@ const form = document.querySelector('#addTaskForm');
 const input = document.querySelector('#txtTaskName');
 const btnDeleteAll = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
-const edit = document.querySelector('#edit');
-
 let itemIndex = 1;
 let items;
 eventListeners();
@@ -11,42 +9,7 @@ function eventListeners() {
     form.addEventListener("submit", addNewItem);
     btnDeleteAll.addEventListener('click', deleteAllItem);
     taskList.addEventListener('click', deleteItem);
-    taskList.addEventListener('click', editItem);
 }
-
-function addNewItem(e) {
-    e.preventDefault();
-
-    if (input.value === '') {
-        alert('Please type a task');
-        return;
-    }
-
-    const li = document.createElement('li');
-    
-    li.classList = itemIndex % 2 == 1 ? 'list-group-item' : 'list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(input.value));
-    li.setAttribute('readonly','readonly');
-
-    const a = document.createElement('a');
-    a.classList = 'delete-item float-end';
-    a.setAttribute('href', '#');
-    a.innerHTML = '<i class="bi bi-x-circle"></i>';
-
-    const b = document.createElement('a');
-    b.classList = 'edit-item float-end';
-    b.setAttribute('href', '#');
-    b.innerHTML = '<i class="bi bi-pencil"></i>';
-    b.setAttribute('style', 'padding-right: 10px');
-    
-    li.appendChild(a);
-    li.appendChild(b);
-
-    taskList.appendChild(li);
-    input.value = '';
-    itemIndex++;
-}
-
 function deleteItem(e) {
     if (e.target.className === 'bi bi-x-circle') {
         e.target.parentElement.parentElement.remove();
@@ -58,39 +21,30 @@ function deleteAllItem(e) {
 
         taskList.innerHTML = '';
     }
+
+
 }
+function addNewItem(e) {
+    e.preventDefault();
 
-function editItem(e) {
-    if (e.target.className === 'bi bi-pencil') {
-        let text;
-        let editedTask = prompt("Please edit your task:", "");
-        if (editedTask == null || editedTask == "") {
-            text = "User cancelled the prompt.";
-        } else {
-  
-            const a = document.createElement('a');
-            a.classList = 'delete-item float-end';
-            a.setAttribute('href', '#');
-            a.innerHTML = '<i class="bi bi-x-circle"></i>';
-
-            const b = document.createElement('a');
-            b.classList = 'edit-item float-end';
-            b.setAttribute('href', '#');
-            b.innerHTML = '<i class="bi bi-pencil"></i>';
-            b.setAttribute('style', 'padding-right: 10px');
-
-            text = editedTask;
-            e.target.parentElement.parentElement.innerHTML = text;
-            e.target.appendChild(a);
-            e.target.appendChild(b);
-
-            taskList.appendChild(e.target);
-            input.value = '';
-            itemIndex++;
-        }
-        
-        
+    if (input.value === '') {
+        alert('Please type a task');
+        return;
     }
-    
-      
+
+    const li = document.createElement('li');
+
+    li.classList = itemIndex % 2 == 1 ? 'list-group-item' : 'list-group-item list-group-item-secondary';
+    li.appendChild(document.createTextNode(input.value));
+
+    const a = document.createElement('a');
+    a.classList = 'delete-item float-end';
+    a.setAttribute('href', '#');
+    a.innerHTML = '<i class="bi bi-x-circle"></i>';
+
+    li.appendChild(a);
+
+    taskList.appendChild(li);
+    input.value = '';
+    itemIndex++;
 }
